3190475637860fa211fd41d9b4feabfe426dd624
Sultanov Mirjamol <9625526+cljamal@users.noreply.github.com>
2023-05-24 01:36:14 +0500

    Update GuidelerController.php

--- src/Classes/GuidelerController.php
+++ src/Classes/GuidelerController.php
@@ -4,6 +4,8 @@ namespace Guideler\Tools\Classes;
 
 use Guideler\Tools\Builders\Table\Builder as TableBuilder;
 use Guideler\Tools\Builders\Table\Resource\Filters;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -24,6 +26,35 @@ class GuidelerController extends BaseController
         });
     }
 
+    public function applyFilters(Builder $model)
+    {
+        $filters = $this->requestFilters();
+        if (count($filters))
+        {
+            $tableFilters = $this->table()->filters()->toArray();
+            foreach ($filters as $filter_key => $val)
+            {
+                if (empty($val))
+                    continue;
+
+                if (isset($tableFilters[$filter_key]))
+                {
+                    $condition = $tableFilters[$filter_key]['condition'] ?? 'eq';
+                    match ($condition) {
+                        'eq', 'sw' => $model->where($filter_key, $val),
+                        'lt' => $model->where($filter_key, '<', $val),
+                        'lte' => $model->where($filter_key, '<=', $val),
+                        'gt' => $model->where($filter_key, '>', $val),
+                        'gte' => $model->where($filter_key, '>=', $val),
+                        'q' => $model->where($filter_key, 'ilike', "%{$val}%"),
+                    };
+                }
+            }
+        }
+        
+        return $model;
+    }
+
     protected function table(): TableBuilder
     {
         return g_table(str(static::class)->replace('\\', '-')->lower()->slug()->toString());
@@ -36,4 +67,16 @@ class GuidelerController extends BaseController
     protected function filters(Filters $filters): void
     {
     }
+
+    private function requestFilters(): array
+    {
+        $acceptedFilters = collect($this->table()->filters()->toArray())->keys()->toArray();
+        $filters = request()->get('filter', []);
+        $filters = collect($filters)->filter(function ($filter, $key) use ($acceptedFilters){
+            return in_array($key, $acceptedFilters);
+        });
+
+        return $filters->toArray();
+    }
+
 }
